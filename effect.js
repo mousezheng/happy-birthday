e403ae5813143fbbdeac8325fd2d05af7294f90b
mousezheng <1486145487@qq.com>
2020-08-15 21:10:32 +0800

    优化

--- effect.js
+++ effect.js
@@ -192,9 +192,9 @@ $('document').ready(function () {
         });
         var i;
         function msgLoop(i) {
-            $("p:nth-child(" + i + ")").fadeOut('slow').delay(1200).promise().done(function () {
+            $("p:nth-child(" + i + ")").fadeOut('slow').delay(500).promise().done(function () {
                 i = i + 1;
-                $("p:nth-child(" + i + ")").fadeIn('slow').delay(3000);
+                $("p:nth-child(" + i + ")").fadeIn('slow').delay(4000);
                 if (i == 50) {
                     $("p:nth-child(49)").fadeOut('slow').promise().done(function () {
                         $('.cake').fadeIn('fast');
@@ -203,7 +203,6 @@ $('document').ready(function () {
                 else {
                     msgLoop(i);
                 }
-
             });
             // body...
         }
